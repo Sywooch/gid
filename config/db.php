0801927dd7ac8f6e36b09d6627a63f4feb0e2780
PaulAsfalt <lavrenuk89@mail.ru>
2014-10-29 23:42:56 +1000

    #первый коммит

--- config/db.php
+++ config/db.php
@@ -1,9 +1,10 @@
 <?php
 
 return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=lavrenop_gid',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
+    'class'       => 'yii\db\Connection',
+    'dsn'         => 'mysql:host=localhost;dbname=gid',
+    'username'    => 'root',
+    'password'    => '',
+    'charset'     => 'utf8',
+    'tablePrefix' => 'jhh_',
 ];
